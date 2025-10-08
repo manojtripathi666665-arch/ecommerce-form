3de7f313b7425f224b3cfd09b9d7ae481cc1865e
manojtripathi666665-arch <manojtripathi666665@gmail.com>
2025-10-08 18:37:46 +0530

    Update router.php

--- router.php
+++ router.php
@@ -1,10 +1,9 @@
 <?php
-// Serve requested file if it exists
-if (php_sapi_name() == 'cli-server') {
-    $file = $_SERVER['DOCUMENT_ROOT'] . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if (php_sapi_name() === 'cli-server') {
+    $file = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     if (is_file($file)) {
         return false;
     }
 }
-// Otherwise serve index.html
 include_once "index.html";
+?>
